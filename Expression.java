6d2b97ad0004ef2c0079e04a3e06ada3d8827466
S-2000-Pawar <85825283+S-2000-Pawar@users.noreply.github.com>
2021-07-09 14:39:48 +0530

    Add files via upload
    Assignment

---
+++ Expression.java
@@ -0,0 +1,10 @@
+//to compute the specified expression and print the output
+
+
+class Expression
+{ 
+   public static void main(String[] arg)
+   {
+     System.out.println((25.5*3.5-3.5*3.5)/(40.5-4.5));
+    }
+}
