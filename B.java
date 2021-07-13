4448e60d4f769c517ab5b97fd7829fe34278a3ab
S-2000-Pawar <85825283+S-2000-Pawar@users.noreply.github.com>
2021-07-13 17:26:43 +0530

    Add files via upload
    Assignment

---
+++ B.java
@@ -0,0 +1,16 @@
+//pattern3
+
+class B
+{
+   public static void main(String[] args)
+   {
+     for(int i=1;i<=5;i++)
+     {
+        for(int j=5;j>=i;j--)
+      {
+         System.out.print("*");
+       }
+          System.out.println();
+      }
+    }
+}
---
+++ I.java
@@ -0,0 +1,33 @@
+//Pattern9
+
+class I
+{
+  
+   public static void main(String[] args)
+   {
+     for(int i=1;i<=5;i++)
+     {
+        for(int j=5;j>i;j--)
+      {
+         System.out.print(" ");
+       }
+         for(int k=1;k<=i;k++)
+          {
+            System.out.print("*");
+          }
+           System.out.println();
+      }
+      for(int i=1;i<=5-1;i++)
+     {
+        for(int j=1;j<=i;j++)
+      {
+         System.out.print(" ");
+       }
+         for(int k=5-1;k>=i;k--)
+          {
+            System.out.print("*");
+          }
+           System.out.println();
+      }
+    }
+}
